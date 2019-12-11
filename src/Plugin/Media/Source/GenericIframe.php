0d3ad1934fabc1e21596c45f5668ddc66608b81d
ambo <andreas.maengel@gmail.com>
2019-12-11 14:43:40 +0100

    fixed namespace

--- src/Plugin/Media/Source/GenericIframe.php
+++ src/Plugin/Media/Source/GenericIframe.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Drupal\blaetter_formatters\Plugin\media\Source;
+namespace Drupal\blaetter_formatters\Plugin\Media\Source;
 
 use Drupal\media\MediaInterface;
 use Drupal\media\MediaSourceBase;
